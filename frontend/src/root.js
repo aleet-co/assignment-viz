664faf3159e67082f44491c558b1ed618babb4b1
tzajac-aleet <t.zajac@aleet.co>
2022-12-07 02:20:06 +0100

    ui: material

--- frontend/src/root.js
+++ frontend/src/root.js
@@ -1,10 +1,51 @@
 import { Outlet } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import MuiAppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
+
+const mdTheme = createTheme();
 
 export default function Root() {
   return (
-    <div>
-      <p>Assignment visualizer</p>
-      <Outlet />
-    </div>
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar />
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
+        >
+          <Toolbar />
+          <Outlet />
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
+
+export const AppBar = () => (
+  <MuiAppBar position="absolute">
+    <Toolbar>
+      <Typography
+        component="h1"
+        variant="h6"
+        color="inherit"
+        noWrap
+        sx={{ flexGrow: 1 }}
+      >
+        Assignment visualizer - Aleet
+      </Typography>
+    </Toolbar>
+  </MuiAppBar>
+);
